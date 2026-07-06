4aedf0a46df1fe08203563269f66474278461d3c
nandinerden1219-alt <nandinerden1219@gmail.com>
2026-07-06 12:57:01 +0800

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,155 @@
-import Image from "next/image";
-
+"use client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Sparkles, RotateCw, FileText, MessageCircle } from "lucide-react";
+import { useRef, useState } from "react";
 export default function Home() {
+  const fileInputRef = useRef(null);
+  const [preview, setPreview] = useState(null);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setPreview(imageUrl);
+    }
+  };
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <div className="flex flex-col flex-1  bg-zinc-50 font-sans dark:bg-black">
+      <main className="font-bold text-lg p-5 border border-b-black">
+        Ai tools
       </main>
+      <div className="flex justify-center items-center p-10">
+        <Tabs defaultValue="overview" className="w-[800px] ">
+          <TabsList>
+            <TabsTrigger value="analysis">Image analysis</TabsTrigger>
+            <TabsTrigger value="recognition">
+              Ingredient recognition
+            </TabsTrigger>
+            <TabsTrigger value="creator">Image creator</TabsTrigger>
+          </TabsList>
+          <TabsContent value="analysis">
+            <Card>
+              <CardHeader>
+                <CardTitle className="flex gap-4 justify-between">
+                  <div className="flex gap-4 font-bold">
+                    <Sparkles /> <h1>Image Analysis</h1>
+                  </div>
+                  <RotateCw />
+                </CardTitle>
+                <CardDescription className="mt-5">
+                  Upload a food photo, and AI will detect the ingredients.
+                </CardDescription>
+                <div>
+                  <input
+                    type="file"
+                    accept="image/*"
+                    ref={fileInputRef}
+                    onChange={handleFileChange}
+                  />
+
+                  {preview && (
+                    <div>
+                      <img
+                        src={preview}
+                        alt="Preview"
+                        style={{ width: "200px", marginTop: "10px" }}
+                      />
+                    </div>
+                  )}
+                </div>
+
+                <div className="flex justify-end mt-4">
+                  <button
+                    type="button"
+                    className="px-6 py-2 rounded-lg font-medium text-white bg-gray-500 "
+                  >
+                    Generate
+                  </button>
+                </div>
+              </CardHeader>
+              <CardContent className="text-sm text-black flex flex-col gap-3">
+                <div className="flex gap-3 items-center font-bold text-xl">
+                  <FileText />
+                  <h1>Here is the summary</h1>
+                </div>
+
+                <p className="text-muted-foreground">
+                  First, enter your image to recognize an ingredients.
+                </p>
+              </CardContent>
+            </Card>
+          </TabsContent>
+          <TabsContent value="recognition">
+            <Card>
+              <CardHeader>
+                <CardTitle className="flex gap-4 justify-between">
+                  <div className="flex gap-4 font-bold">
+                    <Sparkles /> <h1>Ingredient recognition</h1>
+                  </div>
+                  <RotateCw />
+                </CardTitle>
+                <CardDescription className="mt-5">
+                  Describe the food, and AI will detect the ingredients.{" "}
+                </CardDescription>
+                <input type="input" placeholder="Орц оруулах" />
+                <button className="bg-black text-white p-2 rounded-md mt-5">
+                  Generate
+                </button>
+              </CardHeader>
+              <CardContent className="text-sm text-black flex flex-col gap-3">
+                <div className="flex gap-3 items-center font-bold text-xl">
+                  <FileText />
+                  <h1>Identified Ingredients</h1>
+                </div>
+
+                <p className="text-muted-foreground">
+                  First, enter your text to recognize an ingredients.
+                </p>
+              </CardContent>
+            </Card>
+          </TabsContent>
+          <TabsContent value="creator">
+            <Card>
+              <CardHeader>
+                <CardTitle className="flex gap-4 justify-between">
+                  <div className="flex gap-4 font-bold">
+                    <Sparkles /> <h1>Food image creator</h1>
+                  </div>
+                  <RotateCw />
+                </CardTitle>
+                <CardDescription className="mt-5">
+                  What food image do you want? Describe it briefly.{" "}
+                </CardDescription>
+                <input type="file" placeholder="Хоолны тайлбар" />
+                <button className="bg-black text-white p-2 rounded-md mt-5">
+                  Generate
+                </button>
+              </CardHeader>
+              <CardContent className="text-sm text-black flex flex-col gap-3">
+                <div className="flex gap-3 items-center font-bold text-xl">
+                  <FileText />
+                  <h1>Result</h1>
+                </div>
+
+                <p className="text-muted-foreground">
+                  First, enter your text to generate an image.{" "}
+                </p>
+              </CardContent>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
+      <div className="flex justify-end items-end ">
+        <MessageCircle className="text-white bg-black w-fit h-fit p-5 border rounded-[50%]" />
+      </div>
     </div>
   );
 }
